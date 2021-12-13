ea60636ee07629588d0aec06f03ed0edd2ea2071
JulijsO <77743919+JulijsO@users.noreply.github.com>
2021-12-13 10:08:12 +0100

    Add files via upload

---
+++ scripts/slider.js
@@ -0,0 +1,164 @@
+// VARIABELN DEFINIEREN
+// _________________________________________________________
+
+var imgNo = 1;
+var imgNext;
+var count = $(".img-container img").length;
+var count2 = $(".plan-container img").length;
+
+// BILDER
+// ___________________________________________________________
+
+// VORWÄRTS__________________
+
+$(document.body).on("click", ".ar-r-img",function(){
+    console.log("working");
+    
+    if(imgNo == count){
+    imgNext = imgNo;    
+    imgNo = 1;
+    $("#img" + imgNo).css("display", "unset");
+    $("#img" + imgNext).css("display", "none");
+
+    } else { 
+        imgNext = imgNo + 1;
+
+        $("#img" + imgNo).css("display", "none");
+        $("#img" + imgNext).css("display", "unset");
+        imgNo = imgNo + 1;
+        console.log(imgNo);
+        console.log(imgNext);
+    }
+
+});
+
+// RÜCKWERTS__________________
+
+$(document.body).on("click", ".ar-l-img",function(){
+    console.log("working");
+    
+    if(imgNo == 1){
+    imgNext = imgNo;    
+    imgNo = count;
+    $("#img" + imgNo).css("display", "unset");
+    $("#img" + imgNext).css("display", "none");
+
+    } else { 
+        imgNext = imgNo - 1;
+
+        $("#img" + imgNo).css("display", "none");
+        $("#img" + imgNext).css("display", "unset");
+        imgNo = imgNo - 1;
+        console.log(imgNo);
+        console.log(imgNext);
+    }
+
+});
+
+// PLÄNE
+// _______________________________________________________________________
+
+// VORWÄRTS________________
+
+$(document.body).on("click", ".ar-r-pln",function(){
+    
+    if(imgNo == count2){
+    imgNext = imgNo;    
+    imgNo = 1;
+    $("#pln" + imgNo).css("display", "unset");
+    $("#pln" + imgNext).css("display", "none");
+
+    } else { 
+        imgNext = imgNo + 1;
+
+        $("#pln" + imgNo).css("display", "none");
+        $("#pln" + imgNext).css("display", "unset");
+        imgNo = imgNo + 1;
+        console.log(imgNo);
+        console.log(imgNext);
+    }
+
+});
+
+// RÜCKWERTS_________________
+
+$(document.body).on("click", ".ar-l-pln",function(){
+    
+    if(imgNo == 1){
+    imgNext = imgNo;    
+    imgNo = count2;
+    $("#pln" + imgNo).css("display", "unset");
+    $("#pln" + imgNext).css("display", "none");
+
+    } else { 
+        imgNext = imgNo - 1;
+
+        $("#pln" + imgNo).css("display", "none");
+        $("#pln" + imgNext).css("display", "unset");
+        imgNo = imgNo - 1;
+        console.log(imgNo);
+        console.log(imgNext);
+    }
+
+});
+
+
+
+// _____Switch Img/plans_______________________________________________________
+
+$(document.body).on("click", "#img-btn",function(){
+
+    $(".img-container").css("display", "flex");
+    $(".plan-container").css("display", "none");
+    $("#pln" + imgNo).css("display", "none");
+    $("#img" + imgNo).css("display", "none");
+    $("#img1").css("display", "unset");
+    imgNo = 1;
+    console.log(imgNo);
+
+    // line_________
+    $(this).css("border-bottom", "solid 1px");
+    $("#pln-btn").css("border-bottom", "none");
+
+    $("#img-arrows").css("display", "flex");
+    $("#pln-arrows").css("display", "none");
+
+
+});
+
+$(document.body).on("click", "#pln-btn",function(){
+
+    $(".plan-container").css("display", "flex");
+    $(".img-container").css("display", "none");
+    $("#img" + imgNo).css("display", "none");
+    $("#pln" + imgNo).css("display", "none");
+    $("#pln1").css("display", "flex");
+    imgNo = 1;
+    console.log(imgNo);
+
+    // line_________
+    $(this).css("border-bottom", "solid 1px");
+    $("#img-btn").css("border-bottom", "none");
+
+    $("#pln-arrows").css("display", "flex");
+    $("#img-arrows").css("display", "none");
+
+});
+
+
+// _______Reset switch project___________
+
+$(document.body).on("click", ".list-item",function(){
+    imgNo = 1;
+});
+
+
+$(document.body).on("click", ".list-item", function(){setTimeout(reCount, 200)});
+
+
+
+function reCount(){
+    count = $(".img-container img").length;
+    count2 = $(".plan-container img").length;
+    console.log(count);
+}
